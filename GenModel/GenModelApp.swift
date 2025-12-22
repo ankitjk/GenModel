55b651b0ab842dbd29bb102038d7725663b15a0a
ankitjk <43698143+ankitjk@users.noreply.github.com>
2025-12-22 14:20:40 +0800

    Initial Commit

---
+++ GenModel/GenModelApp.swift
@@ -0,0 +1,33 @@
+//
+//  GenModelApp.swift
+//  GenModel
+//
+//  Created by Ankit Kulshrestha on 22/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GenModelApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
